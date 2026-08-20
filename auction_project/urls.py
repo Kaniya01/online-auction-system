307beba6cef5de356d58085b16d3f39d316f1134
Mokshitha Bandhakavi <mokshitha.bandhakavi@gmail.com>
2026-08-20 12:05:06 +0530

    Configure database and media settings
    t

--- auction_project/urls.py
+++ auction_project/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 URL configuration for auction_project project.
 """
@@ -5,6 +6,18 @@ URL configuration for auction_project project.
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+
+
+urlpatterns = [
+"""
+URL configuration for auction_project project.
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,3 +40,41 @@ urlpatterns = [
         name='dashboard_preview'
     ),
 ]
+
+# Serve uploaded media files during development
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+    # Django Admin
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+
+    # Dashboard Preview
+    # This temporarily displays dashboard.html directly.
+    # Later this will be replaced with a proper dashboard view
+    # that retrieves data from PostgreSQL.
+    path(
+        'dashboard-preview/',
+        TemplateView.as_view(
+            template_name='dashboard.html'
+        ),
+        name='dashboard_preview'
+    ),
+]
+=======
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+>>>>>>> a2b8107 (Configure database and media settings)
